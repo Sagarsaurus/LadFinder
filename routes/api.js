e6a46119da3a5ccbaea6e2531362685c3a40897e
Sagarsaurus <sagrsccr@gmail.com>
2014-10-04 18:03:14 -0400

    Commiting node app for basic api

---
+++ routes/api.js
@@ -0,0 +1,34 @@
+var express = require('express');
+var router = express.Router();
+var mongoose = require('mongoose');
+
+mongoose.model("User", {firstName : String, lastName : String, email : String, created_at : {type: Date, default: Date.now}});
+
+var api = {
+    createUser : function(req, res) {
+        var model = mongoose.model('User');
+        var insertContent = req.body;
+        var toPost = new model(insertContent);
+        return toPost.save(function(err, resource) {
+            if (err != null) {
+                res.status(500).send({error:err});
+            }
+
+            return res.send(resource);
+        });
+    },
+
+    getAllUsers : function(req, res) {
+        var model = mongoose.model('User');
+        return model.find({}, function(err, coll) {
+            return res.send(coll);
+        });
+    }
+
+}
+
+router.post('/createUser', api.createUser);
+router.get('/getAllUsers', api.getAllUsers);
+
+module.exports = router;
+
