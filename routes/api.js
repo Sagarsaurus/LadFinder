9ad11a0098c01b28778990219c2310ee2651289a
Sagarsaurus <sagrsccr@gmail.com>
2014-10-20 14:43:46 -0400

    Added capabilities to add friends

--- routes/api.js
+++ routes/api.js
@@ -4,6 +4,12 @@ var mongoose = require('mongoose');
 
 mongoose.model("User", {temp_id : String, username : String, password : String, email : String, phone : String,  created_at : {type: Date, default: Date.now}});
 
+var Friends = new mongoose.Schema({friend : String, phone: String, email : String});
+
+mongoose.model("Friends", Friends);
+
+mongoose.model("Friendships", {username : String, friends : [Friends]});
+
 var api = {
     createUser : function(req, res) {
         var model = mongoose.model('User');
@@ -35,14 +41,30 @@ var api = {
                 return res.send(coll);
             }
         });
+    },
+    addFriend : function(req, res) {
+       var friendShipModel = mongoose.model("Friendships");
+       var friendShip = new friendShipModel(req.body);
+       return friendShipModel.findOne({'username' : req.body.username}, function(err, coll) {
+          if(coll) {
+                 coll.friends.push({friend : req.body.friend, email : req.body.email, phone : req.body.phone});
+                 coll.save();
+                 return res.send(coll);
+          }
+          else {
+              friendShip.save();
+              friendShip.friends.push({friend: req.body.friend, email : req.body.email, phone : req.body.phone});
+              return res.send(friendShip);
+          }
+        });
     }
-
-
 }
+
 
 router.post('/createUser', api.createUser);
 router.get('/getAllUsers', api.getAllUsers);
 router.post('/login', api.login);
+router.post('/addFriend', api.addFriend);
 
 module.exports = router;
 
